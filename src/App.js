0a603e58c6271f6030450dfb062fb96113800799
Matthew Brody <matt@Matthews-MacBook-Air-3.local>
2019-03-21 15:27:37 +1000

    images pull from giphy when search term is entered and they are displayed on the screen in semantic ui cards

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  render() {
+const SearchBar = (props) => {
+  return (
+    <div className="ui input">
+      <input
+        value={props.value} 
+        placeholder="Find the gif of your dreams!" 
+        type="text"
+        onChange={e => props.onSearchInput(e.target.value)}
+      />
+    </div>
+  );
+}
+
+const DisplayGifs = (props) => {
+
+  const gifs = props.data.map(gif => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="column">
+        <div className="ui card" key={gif.id}>
+          <div className="image">
+            <img src={gif.images.fixed_width_downsampled.url} alt={gif.id} />
+          </div>
+        </div>
       </div>
     );
-  }
+  })
+
+  return gifs;
 }
+
+class App extends Component {
+  state = { searchTerm: '', gifs: [] }
+
+  onSearchInput = (term) => {
+    this.setState({ searchTerm: term })
+  }
+
+  onHandleSubmit = async (e) => {
+    e.preventDefault();
+    const gifs = await axios.get('http://api.giphy.com/v1/gifs/search?q=' + this.state.searchTerm + '&api_key=MRLay8p2J7b0XwOwbZOJVCau52Fn4B6R&limit=12')
+    this.setState({ searchTerm: '', gifs: gifs.data.data })
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <h1>Gify Search API</h1>
+        <div className="ui segment">
+          <form onSubmit={this.onHandleSubmit}>
+            <SearchBar 
+              value={this.state.searchTerm}
+              onSearchInput={this.onSearchInput}
+            />
+          </form>
+        </div>
+        <div className="ui three column grid">
+          <DisplayGifs 
+            data={this.state.gifs}
+          />
+        </div>
+      </div>
+    );
+  };
+};
 
 export default App;
